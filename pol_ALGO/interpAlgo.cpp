35bf240916da7616335f4ed70209d3388ca3ce43
Mantej Sokhi <msokhi@Mantej-M3-10.local>
2026-03-15 02:39:27 -0700

    Syntax changes

--- pol_ALGO/interpAlgo.cpp
+++ pol_ALGO/interpAlgo.cpp
@@ -4,6 +4,7 @@
 #include"integerMath.h"
 
 using namespace std;
+// Extra memory b.
 /*
 int newtonInterp(LONG *x, 
                  LONG *y,
@@ -47,45 +48,39 @@ int newtonInterp(LONG *x,
 }
 */
 
+// No extra memory. Use y as OP.
 int newtonInterp(LONG* x,
     LONG* y,
     const int n,
-    const LONG p)
-{
-if (n < 1) return -1;
-
-int d, i, j;
-LONG prod, s;
-
-// Store Newton coefficients directly in y
-for (j = 1; j < n; j++) {
-s = y[0];
-prod = sub64b(x[j], x[0], p);
-
-for (i = 1; i < j; i++) {
-s    = add64b(s, mul64b(prod, y[i], p), p);
-prod = mul64b(prod, sub64b(x[j], x[i], p), p);
+    const LONG p){
+    if(n<1){
+        return -1;
+    }
+    int d;
+    int i;
+    int j;
+    LONG prod;
+    LONG s;
+    for(j=1;j<n;j++){
+        s=y[0];
+        prod=sub64b(x[j],x[0],p);
+        for(i=1;i<j;i++){
+            s=add64b(s,mul64b(prod,y[i],p),p);
+            prod=mul64b(prod,sub64b(x[j],x[i],p),p);
+        }
+        if(prod==0){
+            exit(1);
+        }
+        y[j]=mul64b(sub64b(y[j],s,p),modinv64b(prod,p),p);
 }
-
-if (prod == 0) {
-exit(1);
-}
-
-y[j] = mul64b(sub64b(y[j], s, p), modinv64b(prod, p), p);
-}
-
-// Degree of interpolating polynomial
-d = n - 1;
-while (d >= 0 && y[d] == 0) {
-d--;
-}
-
-// Convert Newton form to standard basis in place
-for (i = 1; i <= d; i++) {
-for (j = d - i; j <= d - 1; j++) {
-y[j] = sub64b(y[j], mul64b(x[d - i], y[j + 1], p), p);
-}
-}
-
-return d;
+    d=n-1;
+    while(d>=0&&y[d]==0){
+        d--;
+    }
+    for(i=1;i<=d;i++){
+        for(j=d-i;j<=d-1;j++){
+            y[j]=sub64b(y[j],mul64b(x[d-i],y[j+1],p),p);
+        }
+    }
+    return d;
 }
