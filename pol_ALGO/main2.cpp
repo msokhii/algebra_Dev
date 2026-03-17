710efc34673b0506a4320e2c2fe1aa01533234c0
mss59 <mss59@cecm-sarah.cecm.sfu.ca>
2026-03-17 10:55:39 -0700

    Added logFile

--- pol_ALGO/main2.cpp
+++ pol_ALGO/main2.cpp
@@ -9,6 +9,8 @@
 #include<chrono>
 #include<iomanip>
 #include<fstream>
+#include<sstream>
+#include<string>
 
 using namespace std; 
 
@@ -106,106 +108,130 @@ int main(){
 long long GLOBALMUL=0;
 long long cpuMUL=0;
 int main(){
+
     LONG p=9223372036854775783;
-    int degN=20;
-    int degD=20;
+    int degN=5;
+    int degD=5;
     const int ITER=100000;
+    const int STEP=3;
 
-    vector<LONG>n(degN+1,0);
-    vector<LONG>d(degD+1,0);
+    ofstream logFile("benchMark.txt");
+    logFile<<"PRIME -> "<<p<<"\n";
+    logFile<<"CALLS -> "<<ITER<<"\n";
+    logFile<<left
+        <<setw(10)<<"STEP"
+        <<setw(10)<<"degN"
+        <<setw(10)<<"degD"
+        <<setw(18)<<"avgTimeNewton"
+        <<setw(18)<<"avgTimeRR"
+        <<setw(14)<<"mulsNewton"
+        <<setw(14)<<"mulsRR"
+        << "\n";
 
-    for(int i=0;i<degN+1;i++){
-        LONG temp=rand64s(p);
-        while(temp==0){
-            temp=rand64s(p);
+    for(int step=1;step<STEP;step++){
+        vector<LONG>n(degN+1,0);
+        vector<LONG>d(degD+1,0);
+
+        for(int i=0;i<degN+1;i++){
+            LONG temp=rand64s(p);
+            while(temp==0){
+                temp=rand64s(p);
+            }
+            n[i]=temp;
         }
-        n[i]=temp;
-    }
-    for(int j=0;j<degD+1;j++){
-        LONG temp=rand64s(p);
-        while(temp==0){
-            temp=rand64s(p);
+        for(int j=0;j<degD+1;j++){
+            LONG temp=rand64s(p);
+            while(temp==0){
+                temp=rand64s(p);
+            }
+            d[j]=temp;
         }
-        d[j]=temp;
-    }
 
-    if(d[degD]!=1){
-        LONG invTerm;
-        invTerm=modinv64b(d[degD],p);
-        for(int i=0;i<=degD;i++){
-            d[i]=mul64b(invTerm,d[i],p);
+        if(d[degD]!=1){
+            LONG invTerm;
+            invTerm=modinv64b(d[degD],p);
+            for(int i=0;i<=degD;i++){
+                d[i]=mul64b(invTerm,d[i],p);
+            }
+            for(int j=0;j<=degN;j++){
+                n[j]=mul64b(invTerm,n[j],p);
+            }
         }
-        for(int j=0;j<=degN;j++){
-            n[j]=mul64b(invTerm,n[j],p);
+
+        vector<LONG>nCopy=n;
+        vector<LONG>dCopy=d;
+
+        int m=degN+degD+1;
+        vector<LONG>x(m,0);
+        for(int i=0;i<m;i++){
+            x[i]=i+1;
         }
-    }
 
-    vector<LONG>nCopy=n;
-    vector<LONG>dCopy=d;
-
-    int m=degN+degD+1;
-    vector<LONG>x(m,0);
-    for(int i=0;i<m;i++){
-        x[i]=i+1;
-    }
-
-    vector<LONG>y(m,0);
-    for(int i=0;i<m;i++){
-        LONG denEval=pEVAL64(d.data(),degD,x[i],p);
-        if(denEval==0){
-            return -1;
+        vector<LONG>y(m,0);
+        for(int i=0;i<m;i++){
+            LONG denEval=pEVAL64(d.data(),degD,x[i],p);
+            if(denEval==0){
+                return -1;
+            }
+            LONG numEval=pEVAL64(n.data(),degN,x[i],p);
+            y[i]=mul64b(numEval,modinv64b(denEval,p),p);
         }
-        LONG numEval=pEVAL64(n.data(),degN,x[i],p);
-        y[i]=mul64b(numEval,modinv64b(denEval,p),p);
-    }
 
-    dispVEC64(y);
-    vector<LONG>yCopy(m,0);
-    copy(y.begin(),y.end(),yCopy.begin());
-    int degU=newtonInterp(x.data(),yCopy.data(),m,p);
-    cout<<"Degree of the IP: "<<degU<<"\n";
-    GLOBALMUL=0;
-    auto start=chrono::steady_clock::now();
-    for(int i=0;i<ITER;i++){
+        vector<LONG>yCopy(m,0);
         copy(y.begin(),y.end(),yCopy.begin());
         int degU=newtonInterp(x.data(),yCopy.data(),m,p);
-    };
-    auto stop=chrono::steady_clock::now();
-    double total=chrono::duration<double,std::micro>(stop-start).count();
-    double avgTimeNewton=total/ITER;
-    cout<<avgTimeNewton<<"\n";
-    long long newtonMuls=GLOBALMUL/ITER;
-    cout<<newtonMuls<<"\n";
-    vector<LONG>M(m+1,0);
-    M[0]=1;
-    int degM=mkM(M,x,p);
+        GLOBALMUL=0;
+        auto start=chrono::steady_clock::now();
+        for(int i=0;i<ITER;i++){
+            copy(y.begin(),y.end(),yCopy.begin());
+            int degU=newtonInterp(x.data(),yCopy.data(),m,p);
+        };
+        auto stop=chrono::steady_clock::now();
+        auto cpStart=chrono::steady_clock::now();
+        for(int i=0;i<ITER;i++){
+            copy(y.begin(),y.end(),yCopy.begin());
+        }
+        auto cpStop=chrono::steady_clock::now();
+        double cpTotal=chrono::duration<double,std::micro>(cpStop-cpStart).count();
+        double total=chrono::duration<double,std::micro>(stop-start).count();
+        double avgTimeCp=cpTotal/ITER;
+        double avgTimeNewton=(total/ITER)-avgTimeCp;
+        long long newtonMuls=GLOBALMUL/ITER;
+        vector<LONG>M(m+1,0);
+        M[0]=1;
+        int degM=mkM(M,x,p);
 
-    dispVEC64(n);
-    dispVEC64(d);
-    dispVEC64(x);
-    dispVEC64(y);
-    dispVEC64(M); 
+        RatReconFastWS W(degM);
+        vector<LONG>rOut(m,0);
+        vector<LONG>tOut(m,0);
+        int degR=-1;
+        int degT=-1;
+        int flag=-999;
+        GLOBALMUL=0;
+        auto start2=chrono::steady_clock::now();
+        for(int k=0;k<ITER;k++){
+            flag=ratReconFastKernelWS(M,y,m,degU,
+            degN,degD,p,W,rOut.data(),degR,tOut.data(),degT);
+        }
+        auto stop2=chrono::steady_clock::now();
+        long long RRmuls=GLOBALMUL/ITER;
+        long long cpuMULRR=cpuMUL/ITER;
+        double total2=chrono::duration<double,std::micro>(stop2-start2).count();
+        double avgTimeRR=total2/ITER;
+        
+        logFile<<left<<
+                 setw(10)<<step<<
+                 setw(10)<<degN<<
+                 setw(10)<<degD<<
+                 setw(18)<<avgTimeNewton<<
+                 setw(18)<<avgTimeRR<<
+                 setw(14)<<newtonMuls<<
+                 setw(14)<<RRmuls+cpuMULRR<<"\n";
 
-    RatReconFastWS W(degM);
-    vector<LONG>rOut(m,0);
-    vector<LONG>tOut(m,0);
-    int degR=-1;
-    int degT=-1;
-    int flag=-999;
-    GLOBALMUL=0;
-    auto start2=chrono::steady_clock::now();
-    for(int k=0;k<ITER;k++){
-        flag=ratReconFastKernelWS(M,y,m,degU,
-        degN,degD,p,W,rOut.data(),degR,tOut.data(),degT);
+        degN*=2;
+        degD*=2;
     }
-    auto stop2=chrono::steady_clock::now();
-    long long RRmuls=GLOBALMUL/ITER;
-    long long cpuMULRR=cpuMUL/ITER;
-    double total2=chrono::duration<double,std::micro>(stop2-start2).count();
-    double avgTimeRR=total2/ITER;
-    cout<<avgTimeRR<<"\n";
-    cout<<RRmuls<<"\n";
-    cout<<cpuMULRR<<"\n";
+    logFile.close();
     return 0;
 }
 
